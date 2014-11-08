47037391b841b6e3bbc6c1226d9b10b3384e398c
Woody Gilk <woody.gilk@gmail.com>
2014-11-08 09:09:05 -0600

    Remove Arr::range, PHP has range() function, refs #8

--- src/Arr.php
+++ src/Arr.php
@@ -197,30 +197,6 @@ class Arr
     }
 
     /**
-     * Fill an array with a range of numbers.
-     *
-     *     // Fill an array with values 5, 10, 15, 20
-     *     $values = Arr::range(5, 20);
-     *
-     * @param   integer $step   stepping
-     * @param   integer $max    ending number
-     * @return  array
-     */
-    public static function range($step = 10, $max = 100)
-    {
-        if ($step < 1) {
-            return array();
-        }
-
-        $array = array();
-        for ($i = $step; $i <= $max; $i += $step) {
-            $array[$i] = $i;
-        }
-
-        return $array;
-    }
-
-    /**
      * Retrieve a single key from an array. If the key does not exist in the
      * array, the default value will be returned instead.
      *
